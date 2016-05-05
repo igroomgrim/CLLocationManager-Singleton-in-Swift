fccdad62cac86fdb416f09c26f569215bd053994
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-05 22:46:31 +0700

    initial sample project and gitignore file

---
+++ Tracing/Tracing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tracing
+//
+//  Created by Anak Mirasing on 5/5/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
